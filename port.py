87af7779ee416d9f1a06b8e1f528c0bba6fd4f87
Maddy Mangipudi <mangipudi@gmail.com>
2018-04-07 22:42:10 +0800

    Reformatted code

--- port.py
+++ port.py
@@ -8,37 +8,43 @@ with open("./resources/trades/msltrades.csv", 'r') as f:
     trades = []
     reader = csv.reader(f)
     for row in reader:
-        trade = (datetime.strptime(row[0],'%d-%b-%Y'), row[1], row[3], row[4], row[5], Decimal(row[6]), Decimal(row[14]))
+        trade = (
+        datetime.strptime(row[0], '%d-%b-%Y'), row[1], row[3], row[4], row[5], Decimal(row[6]), Decimal(row[14]))
         trades.append(trade)
 
-    trades.sort(key= lambda t: t[0])
-    #print(trades)
+    trades.sort(key=lambda t: t[0])
+    # print(trades)
     for trade in trades:
         if trade[1] in position:
-            if position[trade[1]]["open_side"] ==  trade[2]:
-                #Add to Existing position
+            if position[trade[1]]["open_side"] == trade[2]:
+                # Add to Existing position
                 position[trade[1]]["open_value"] += trade[5]
                 position[trade[1]]["open_charges"] += trade[6]
-                trade_value = position[trade[1]]["open_value"] if trade[2] == "Sell" else -position[trade[1]]["open_value"]
+                trade_value = position[trade[1]]["open_value"] if trade[2] == "Sell" else -position[trade[1]][
+                    "open_value"]
                 pnl = trade_value - position[trade[1]]["open_charges"]
                 position[trade[1]]["pnl"] = pnl;
                 continue
             else:
-                #Closing position
+                # Closing position
                 position[trade[1]]["close_date"] = trade[0]
                 position[trade[1]]["close_side"] = trade[2]
                 position[trade[1]]["close_value"] += trade[5]
                 position[trade[1]]["close_charges"] += trade[6]
-                if position[trade[1]]["close_value"] != 0 :
+                if position[trade[1]]["close_value"] != 0:
                     if trade[2] == "Sell":
-                        position[trade[1]]["pnl"] = position[trade[1]]["close_value"] - position[trade[1]]["open_value"] - (position[trade[1]]["open_charges"] + position[trade[1]]["close_charges"])
+                        position[trade[1]]["pnl"] = position[trade[1]]["close_value"] - position[trade[1]][
+                            "open_value"] - (position[trade[1]]["open_charges"] + position[trade[1]]["close_charges"])
                     elif trade[2] == "Buy":
-                        position[trade[1]]["pnl"] = position[trade[1]]["open_value"] - position[trade[1]]["close_value"] - (position[trade[1]]["open_charges"] + position[trade[1]]["close_charges"])
+                        position[trade[1]]["pnl"] = position[trade[1]]["open_value"] - position[trade[1]][
+                            "close_value"] - (position[trade[1]]["open_charges"] + position[trade[1]]["close_charges"])
                     else:
                         print("UNKNOWN SIDE")
         else:
-            position[trade[1]] = {"open_date":trade[0], "open_side": trade[2], "open_value":trade[5], "open_charges": trade[6],
-                                  "close_date": datetime.now(), "close_side": "Close OUT", "close_value": 0, "close_charges":  0}
+            position[trade[1]] = {"open_date": trade[0], "open_side": trade[2], "open_value": trade[5],
+                                  "open_charges": trade[6],
+                                  "close_date": datetime.now(), "close_side": "Close OUT", "close_value": 0,
+                                  "close_charges": 0}
             pnl = 0
             if trade[2] == "Sell":
                 pnl = trade[5] - trade[6]
@@ -48,13 +54,12 @@ with open("./resources/trades/msltrades.csv", 'r') as f:
                 print("UNKNOWN SIDE")
             position[trade[1]]["pnl"] = pnl;
 
-
-    #print(position)
+    # print(position)
     net_pnl = 0
     for key in position:
         pnl = position[key]["pnl"]
         net_pnl += pnl
-        print(key + "," + position[key]["open_date"].strftime('%d/%m/%Y')+ "," ,  str(position[key]["open_value"]) + "," , str(position[key]["open_charges"]) + "," , position[key]["close_date"].strftime('%d/%m/%Y') + ","
-              , str(position[key]["close_value"]) + "," , str(position[key]["close_charges"]) + "," , pnl)
+        print(key + "," + position[key]["open_date"].strftime('%d/%m/%Y') + ",", str(position[key]["open_value"]) + ",",
+              str(position[key]["open_charges"]) + ",", position[key]["close_date"].strftime('%d/%m/%Y') + ","
+              , str(position[key]["close_value"]) + ",", str(position[key]["close_charges"]) + ",", pnl)
     print(net_pnl)
-
